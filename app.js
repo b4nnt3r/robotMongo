2fb5585eff51e4b06a593c46324916c35cfc0e85
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-03 15:29:33 -0400

    cleaned up mongoClient

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const passport = require('passport');
+const passport = require('passport');
 const mustacheExpress = require('mustache-express');
 let MongoClient = require('mongodb').MongoClient;
 let url = 'mongodb://127.0.0.1:27017/robotdb';
@@ -13,75 +13,76 @@ app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
-let findAll = function(db, callback) {
-  let collection = db.collection('robots');
-  collection.find().toArray(function(err, result) {
-    console.log("found", result.length, "robots");
-    callback(result);
-  });
-}
-
-let findJobless = function(db, callback) {
-  let collection = db.collection('robots');
-  collection.find({
-    "job": null
-  }).toArray(function(err, result) {
-    console.log("found", result.length, "robots");
-    callback(result);
-  });
-}
-
-let findEmployed = function(db, callback) {
-  let collection = db.collection('robots');
-  collection.find({
-    "company": {
-      $ne: null
-    }
-  }).toArray(function(err, result) {
-    console.log("found " + result.length + " robots");
-    callback(result);
-  });
-}
-
-app.get('/', function(request, response) {
+let findAll = function(callback) {
   MongoClient.connect(url, function(err, db) {
-    findAll(db, function(result) {
-      response.render('index', {
-        robots: result
-      });
+
+    let collection = db.collection('robots');
+    collection.find().toArray(function(err, result) {
+      console.log("found", result.length, "robots");
+      callback(result);
+    });
+  });
+}
+
+let findJobless = function(callback) {
+  MongoClient.connect(url, function(err, db) {
+
+    let collection = db.collection('robots');
+    collection.find({
+      "job": null
+    }).toArray(function(err, result) {
+      console.log("found", result.length, "robots");
+      callback(result);
+    });
+  });
+}
+
+let findEmployed = function(callback) {
+  MongoClient.connect(url, function(err, db) {
+
+    let collection = db.collection('robots');
+    collection.find({
+      "company": {
+        $ne: null
+      }
+    }).toArray(function(err, result) {
+      console.log("found " + result.length + " robots");
+      callback(result);
+    });
+  });
+}
+
+app.get('/', function(req, res) {
+  findAll(function(result) {
+    res.render('index', {
+      robots: result
     });
   });
 });
 
-app.get('/afh', function(request, response) {
-  MongoClient.connect(url, function(err, db) {
-    findJobless(db, function(result) {
-      response.render('index', {
-        robots: result
-      });
+app.get('/afh', function(req, res) {
+  findJobless(function(result) {
+    res.render('index', {
+      robots: result
     });
   });
 });
 
-app.get('/employed', function(request, response) {
-  MongoClient.connect(url, function(err, db) {
-    findEmployed(db, function(result) {
-      response.render('index', {
-        robots: result
-      });
+app.get('/employed', function(req, res) {
+  findEmployed(function(result) {
+    res.render('index', {
+      robots: result
     });
   });
 });
 
 app.get('/:id', function(req, res) {
-  MongoClient.connect(url, function(err, db) {
-    findAll(db, function(result) {
-      let robot = result.find(function(element) {
-        return element.username.toLowerCase() === req.params.id;
-      });
-      res.render("details", {
-        robots: robot
-      });
+  findAll(function(result) {
+    let robot = result.find(function(element) {
+      return element.username.toLowerCase() === req.params.id;
+    });
+    res.render("details", {
+      robots: robot
     });
   });
 });
